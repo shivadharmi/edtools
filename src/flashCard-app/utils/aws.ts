c374e3c5123177d3ea1e007c4f00028d35247ef4
siva sankar bogala <myshiva59@gmail.com>
2021-01-14 12:30:41 +0530

    testing for aws keys not being loaded when deployed to netlify

--- src/flashCard-app/utils/aws.ts
+++ src/flashCard-app/utils/aws.ts
@@ -1,57 +1,60 @@
 /// <reference types="aws-sdk" />
 import AWS from "aws-sdk";
 export const uploadToS3 = (
-  data: string | Blob,
-  contentType: string,
-  fileName: string,
+    data: string | Blob,
+    contentType: string,
+    fileName: string,
 ) => {
-  AWS.config.update({
-    region: "ap-south-1",
-    credentials: {
-        accessKeyId: process.env.REACT_APP_EDTOOLS_AWS_ACCESS_KEY_ID as string,
-        secretAccessKey: process.env.REACT_APP_EDTOOLS_AWS_SECRET_ACCESS_KEY as string,
-      } ,
-  });
-
-  const s3Bucket = new AWS.S3({ params: { Bucket: "excalifcbuck" } });
-  let buf;
-  let contentEncoding;
-  if (typeof data === "string") {
-    buf = Buffer.from(data.replace(/^data:image\/\w+;base64,/, ""), "base64");
-    contentEncoding = "base64";
-  } else {
-    buf = data;
-    contentEncoding = "blob";
-  }
-  const params = {
-    Key: `${fileName}`,
-    Body: buf,
-    ContentEncoding: contentEncoding,
-    ContentType: contentType,
-    Bucket: "excalifcbuck",
-    ACL: "public-read",
-  };
-  const progress = document.getElementById("progress_inner")!;
-  const actionText = document.getElementById("action_text")!;
-  progress.style.display = "block";
-  actionText.style.display = "block";
-  const upload = s3Bucket
-    .putObject(params)
-    .on("httpUploadProgress", (eve) => {
-      const percentage = (eve.loaded / eve.total) * 100;
-      progress.style.width = `${percentage}%`;
-      console.log();
-    })
-    .send((err, data) => {
-      if (data) {
-        progress.style.display = "none";
-        const link = `https://excalifcbuck.s3.ap-south-1.amazonaws.com/${fileName}`;
-        actionText.innerHTML = link;
-        window.localStorage.setItem("link", link);
-        // alert("Successfully uploaded photo.");
-      } else {
-        console.log(err);
-        alert(err.message);
-      }
+    AWS.config.update({
+        apiVersion: "2006-03-01",
+        signatureVersion:"V4",
+        region: "ap-south-1",
+        credentials: {
+            accessKeyId: process.env.REACT_APP_EDTOOLS_AWS_ACCESS_KEY_ID as string,
+            secretAccessKey: process.env.REACT_APP_EDTOOLS_AWS_SECRET_ACCESS_KEY as string,
+        },
     });
+
+    console.log(process.env.REACT_APP_EDTOOLS_AWS_ACCESS_KEY_ID, process.env.REACT_APP_EDTOOLS_AWS_SECRET_ACCESS_KEY);
+    const s3Bucket = new AWS.S3({ params: { Bucket: "excalifcbuck" } });
+    let buf;
+    let contentEncoding;
+    if (typeof data === "string") {
+        buf = Buffer.from(data.replace(/^data:image\/\w+;base64,/, ""), "base64");
+        contentEncoding = "base64";
+    } else {
+        buf = data;
+        contentEncoding = "blob";
+    }
+    const params = {
+        Key: `${fileName}`,
+        Body: buf,
+        ContentEncoding: contentEncoding,
+        ContentType: contentType,
+        Bucket: "excalifcbuck",
+        ACL: "public-read",
+    };
+    const progress = document.getElementById("progress_inner")!;
+    const actionText = document.getElementById("action_text")!;
+    progress.style.display = "block";
+    actionText.style.display = "block";
+    const upload = s3Bucket
+        .putObject(params)
+        .on("httpUploadProgress", (eve) => {
+            const percentage = (eve.loaded / eve.total) * 100;
+            progress.style.width = `${percentage}%`;
+            console.log();
+        })
+        .send((err, data) => {
+            if (data) {
+                progress.style.display = "none";
+                const link = `https://excalifcbuck.s3.ap-south-1.amazonaws.com/${fileName}`;
+                actionText.innerHTML = link;
+                window.localStorage.setItem("link", link);
+                // alert("Successfully uploaded photo.");
+            } else {
+                console.log(err);
+                alert(err.message);
+            }
+        });
 };
